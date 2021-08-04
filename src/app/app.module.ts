9415aabdec5a2de6e213c64214c067cd1f613e11
ErikMarkosyan <er001.em@gmail.com>
2021-08-04 23:47:01 +0400

    providers view providers & reslution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ProvidersViewProvidersModule } from './providers-view-providers/providers-view-providers.module';
+import { ResolutionModifiersModule } from './resolution-modifiers/resolution-modifiers.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProvidersViewProvidersModule,
+    ResolutionModifiersModule
+  
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+  ],
+  bootstrap: [AppComponent],
+  providers: []
 })
 export class AppModule { }
